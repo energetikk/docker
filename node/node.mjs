e0f6b13ca4b36114f7aa6194333afee6efb646e9
Pavel Demin <pavlikdemin@ya.ru>
2024-06-17 00:35:59 +0300

    add time node to 20sec

--- node/node.mjs
+++ node/node.mjs
@@ -4,4 +4,4 @@ fs.appendFile('my_file.txt', 'Файл создан Node.js', (err) => {
     if (err) throw errconsole.log('Файл успешно созранен')
 })
 
-setTimeout(() => console.log('Задержка'), 10000)
+setTimeout(() => console.log('Задержка'), 20000)
