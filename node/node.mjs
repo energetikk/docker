572ad229a5c61912c249de893b964b7aeb0525da
Pavel Demin <pavlikdemin@ya.ru>
2024-06-16 23:52:41 +0300

    initial commit

---
+++ node/node.mjs
@@ -0,0 +1,7 @@
+import fs from 'fs'
+
+fs.appendFile('my_file.txt', 'Файл создан Node.js', (err) => {
+    if (err) throw errconsole.log('Файл успешно созранен')
+})
+
+setTimeout(() => console.log('Задержка'), 10000)
---
+++ python/main.py
@@ -0,0 +1,10 @@
+import calendar
+
+print('Добро пожаловать!')
+
+year = int(input('Пожалуйста введите год:'))
+month = int(input('Пожалуйста введите номер любого месяца:'))
+
+print(calendar.month(year, month))
+
+print('Адьёс, Амигос! До новых встреч')
